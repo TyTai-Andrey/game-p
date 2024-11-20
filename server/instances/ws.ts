53592f6297478354dc3a457b241aa638eed96bbf
andriyanov.a <andriyanov.andrey.al@gmail.com>
2024-11-20 17:36:25 +0300

    update ExpressWs

--- server/instances/ws.ts
+++ server/instances/ws.ts
@@ -1,5 +1,13 @@
 import expressWs from 'express-ws';
-import { WebSocketServerWithPatchClient } from '../utils/ws-operations.js';
+
+type WebSocket = Parameters<expressWs.WebsocketRequestHandler>[0] & {
+  clientId?: string
+  gameId?: string
+  friendId?: string
+}
+
+type WebSocketServer = ReturnType<expressWs.Instance['getWss']>
+type WebSocketServerWithPatchClient = Omit<WebSocketServer, 'clients'> & { clients: Set<WebSocket> }
 
 class ExpressWs {
   static instance: ExpressWs;
@@ -16,16 +24,62 @@ class ExpressWs {
 
     ExpressWs.instance = this;
 
-    return this;
+    return ExpressWs.instance;
   }
 
-  getWss(): WebSocketServerWithPatchClient | undefined {
-    return this.expressWsApp?.getWss();
+  getExpressWsApp() {
+    return ExpressWs.instance.expressWsApp as expressWs.Instance;
+  }
+
+  getWss(): WebSocketServerWithPatchClient {
+    return ExpressWs.instance.expressWsApp?.getWss() as WebSocketServerWithPatchClient;
   }
 
   getWs() {
-    return this.expressWsApp?.app;
+    return ExpressWs.instance.expressWsApp?.app;
+  }
+
+  sendForClientByFuncCondition(func: (client: WebSocket) => boolean, data: any) {
+    ExpressWs.instance.getWss().clients.forEach((client) => {
+      if (func(client)) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  sendForAllClient(ws: WebSocket, data: any) {
+    ExpressWs.instance.getWss().clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  sendForThisClient(ws: WebSocket, data: any) {
+    ExpressWs.instance.getWss().clients.forEach((client) => {
+      if (client === ws && client.readyState === ws.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  sendForClientById(ws: WebSocket, data: any, id: string) {
+    ExpressWs.instance.getWss().clients.forEach((client) => {
+      if (client.clientId === id && client.gameId === ws.gameId) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  getClientHelpers(ws: WebSocket) {
+    return {
+      sendForThisClient: (data: any) => ExpressWs.instance.sendForThisClient(ws, data),
+      sendForClientById: (data: any, id: string) => ExpressWs.instance.sendForClientById(ws, data, id),
+      sendForClientByFuncCondition: (func: (client: WebSocket) => boolean, data: any) => ExpressWs.instance.sendForClientByFuncCondition(func, data),
+      sendForAllClient: (data: any) => ExpressWs.instance.sendForAllClient(ws, data),
+    };
   }
 }
 
+export type { WebSocket, WebSocketServerWithPatchClient };
 export default ExpressWs;
