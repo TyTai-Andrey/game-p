0a570e5e57d065f91b98cfca85875cedc0e8b79c
andriyanov.a <andriyanov.andrey.al@gmail.com>
2024-11-22 00:18:01 +0300

    update css

--- client/src/pages/Game/Aside/History/index.tsx
+++ client/src/pages/Game/Aside/History/index.tsx
@@ -6,11 +6,13 @@ import useStore from '@store/index';
 
 // components
 import HistoryItem from '@pages/Game/Aside/HistoryItem';
+import classNames from 'classnames';
 
 const History = () => {
   const history = useStore(state => state.history);
+  const isOnline = useStore(state => state.isOnline);
   return (
-    <div className={styles.root}>
+    <div className={classNames(styles.root, { [styles.isOnline]: isOnline })}>
       <h1>История ходов</h1>
       <div className={styles.historyTurns}>
         {
